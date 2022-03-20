4ab7b157a7b133159a43a3bac2c8037711d97d3b
sasha57_179 <alexander.latyshev@silvermont.team>
2022-03-20 12:13:30 +0000

    Fix bugs

--- py/main.py
+++ py/main.py
@@ -8,11 +8,13 @@ import os
 def parse_args():
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--site', '-s', help='site to go', type=int, required=True)
-    parser.add_argument('--address', '-a', help='???', type=int, required=True)
+    parser.add_argument('--machine', '-m', help='machine to send request to', type=int, required=True)
+    
+    parser.add_argument('--address', '-a', help='ip to find it\'s site', type=int, required=False)
+    parser.add_argument('--site', '-s', help='site to find it\'s ip', type=int, required=False)
+    
     parser.add_argument('--owner', '-o', help='owner of the message', type=int, required=True)
-    parser.add_argument('--key-name', '-k', help='name of the key for hash', type=str, required=True)
-    parser.add_argument('--signature', '-s', help='unique owner signature', type=str, required=True)
+    parser.add_argument('--key-name', '-k', help='name of the key for hash', dest='key_name', type=str, required=True)
     parser.add_argument('--timeout', '-t', help='request timeout', type=int, required=True)
 
     return parser.parse_args()
@@ -22,10 +24,16 @@ def main():
     
     request_params = {
         "owner": args.owner,
-        "expire": args.expire,
     }
     
-    key = os.environ.get(args.key)
+    if args.address is not None:
+        request_params["address"] = args.address
+    else if args.site is not None:
+        request_params["site"] = args.site
+    else:
+        assert False
+    
+    key = os.environ.get(args.key_name)
     hash_key = hmac.new(key, request_params, hashlib.sha256).hexdigest()
     print(hash_key)
     
@@ -33,7 +41,7 @@ def main():
     request_params["timestamp"] = current_timestamp
     request_params["key"] = hash_key
     
-    request = requests.post(args.site, json=request_params, timeout=args.timeout)
+    request = requests.post(args.machine, json=request_params, timeout=args.timeout)
     print(f"Status: {request.status_code}.")
     print(f"Request: {request.json()}.")
     
